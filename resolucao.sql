afb035506c373927d13afc9af6694328d6f3f187
pereira4ngel <42014734+pereira4ngel@users.noreply.github.com>
2019-12-04 18:37:41 -0300

    Update resolucao.sql

--- resolucao.sql
+++ resolucao.sql
@@ -1,4 +1,11 @@
 /*Resoluções*/
+ultimo sabado
+
+select (( date_part('day', datahora)  +  (2 * date_part('month', datahora)) + (3 * (date_part('month', datahora)+1) / 5 ) + 
+date_part('year', datahora) +  (date_part('year', datahora) / 4) - (date_part('year', datahora) / 100) + (date_part('year', datahora) / 100) + 2)/ 7)::integer % 7 as diasemana
+
+
+
 --1) Atribuir uma cesta de 3 pontos ao jogador Joao Palito do time Mosqueteiro Azul.
 
 INSERT INTO jogojogador(jogo, jogador, cesta2, cesta3, tiroslivres)
@@ -8,13 +15,20 @@ join time on jogador.time = time.codigo
 WHERE lower(jogador.nome) like 'joao palito' and lower(time.nome) like 'mosqueteiro azul'
 
 
-
 --3) Mostrar os jogadores que pontuaram em todos os jogos de seus times.
 select jogador.nome
 from jogojogador
 join jogador on jogojogador.jogador = jogador.codigo
 join time on jogador.time = time.codigo
 where (cesta2 > 0) or (cesta3 > 0) or (tiroslivres > 0);
+
+--4) --Mostrar os jogadores que fizeram cestas de 3 pontos em 3 jogos consecutivos de seus times.
+select  jogador.nome from jogo
+join jogojogador on jogo.codigo = jogojogador.jogo
+join jogador on jogojogador.jogador = jogador.codigo
+where cesta3 = 3
+order by jogo.datahora desc limit 3
+
 
 --5) Mostrar o placar final de cada jogo.
 select time.nome, count(cesta2) as cesta2, count(cesta3) as cesta3, count(tiroslivres) as tiroslivres
